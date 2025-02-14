af3dca704765f182702ce540c8838ce66ea50ec3
Ramiro Estigarribia <ramiroec@gmail.com>
2025-02-14 13:29:41 -0300

    Mejorar la legibilidad y los mensajes de consola en el proceso ETL y las rutas de la API

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,120 @@
-import { CronJob } from 'cron'
-import { URL_ROOT, CRON_SCHEDULE, TIMEZONE, PORT, prisma } from './config'
-import axios from 'axios'
-import AdmZip from 'adm-zip'
-import express, { Express } from 'express'
-import { Contribuyente } from './types'
-import router from './routes'
+import { CronJob } from 'cron';
+import { URL_ROOT, CRON_SCHEDULE, TIMEZONE, PORT, prisma } from './config';
+import axios from 'axios';
+import AdmZip from 'adm-zip';
+import express, { Express } from 'express';
+import { Contribuyente } from './types';
+import router from './routes';
 
-const startmeup = process.argv[2] === 'startmeup'
+// Verifica si se pasó el argumento 'startmeup' al ejecutar el script
+const startmeup = process.argv[2] === 'startmeup';
 
+// Configura el trabajo cron para ejecutar el proceso ETL en un horario específico
 const etlSchedule = new CronJob(
   CRON_SCHEDULE,
-	startETL,
-	null,
-	false,
-	TIMEZONE
-)
-const app: Express = express()
+  startETL,
+  null,
+  false,
+  TIMEZONE
+);
+
+// Inicializa la aplicación Express
+const app: Express = express();
 app.use(router);
 
-main()
+// Función principal que inicia el servidor web y el proceso ETL
+main();
 
 /**
- * Downloads and parses a zip file based on the ending digit.
- * @param endingDigit - The ending digit used to determine the zip file to download.
- * @returns An array of `Contribuyente` objects parsed from the downloaded zip file.
+ * Descarga y parsea un archivo ZIP basado en un dígito final.
+ * @param endingDigit - El dígito final usado para determinar el archivo ZIP a descargar.
+ * @returns Un array de objetos `Contribuyente` parseados del archivo ZIP descargado.
  */
 async function downloadAndParseZip(endingDigit: number): Promise<Contribuyente[]> {
-  console.log('Downloading zip, and parsing data for ending digit: ', endingDigit)
-  const fechaHoraImportacion = new Date().toISOString()
-  const url = `${URL_ROOT}${endingDigit}.zip`
-  const response = await axios.get(url, { responseType: 'arraybuffer' })
-  const zip = new AdmZip(Buffer.from(response.data, 'binary'))
-  const zipEntries = zip.getEntries()
-  const txtFile = zipEntries[0]
+  console.log('Descargando ZIP y parseando datos para el dígito final: ', endingDigit);
+  const fechaHoraImportacion = new Date().toISOString();
+  const url = `${URL_ROOT}${endingDigit}.zip`;
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  const zip = new AdmZip(Buffer.from(response.data, 'binary'));
+  const zipEntries = zip.getEntries();
+  const txtFile = zipEntries[0];
+
   if (!txtFile) {
-    console.log('No txt file found in zip')
+    console.log('No se encontró un archivo TXT en el ZIP');
     return [];
   }
-  const content = txtFile.getData().toString('utf8');
 
-  const lines = content.split('\n')
+  const content = txtFile.getData().toString('utf8');
+  const lines = content.split('\n');
+
   const dataParsed = lines.map(line => {
-    const [ruc, razonSocial, digitoVerificador, rucAnterior, estado] = line.split('|')
-    if (!ruc || !razonSocial || !digitoVerificador ) {
-      return undefined
+    const [ruc, razonSocial, digitoVerificador, rucAnterior, estado] = line.split('|');
+    if (!ruc || !razonSocial || !digitoVerificador) {
+      return undefined;
     } else {
-      return { ruc, razonSocial, digitoVerificador, rucAnterior, estado, fechaHoraImportacion }
+      return { ruc, razonSocial, digitoVerificador, rucAnterior, estado, fechaHoraImportacion };
     }
-  })
-  const data: Contribuyente[] = dataParsed.filter((item): item is Contribuyente => item !== undefined)
-  return data
+  });
+
+  const data: Contribuyente[] = dataParsed.filter((item): item is Contribuyente => item !== undefined);
+  return data;
 }
 
-
 /**
- * Stores the given array of `Contribuyente` objects in the database.
- * @param data - The array of `Contribuyente` objects to store.
+ * Almacena un array de objetos `Contribuyente` en la base de datos.
+ * @param contribuyentes - El array de objetos `Contribuyente` a almacenar.
  */
 async function storeData(contribuyentes: Contribuyente[]) {
-  console.log('Storing data...')
+  console.log('Almacenando datos...');
   for (const item of contribuyentes) {
     try {
       await prisma.contribuyente.upsert({
         where: { ruc: item.ruc },
         update: item,
         create: item,
-      })
+      });
     } catch (error) {
-      console.log('Error storing item: ', item, error)
-      break
+      console.log('Error almacenando el item: ', item, error);
+      break;
     }
   }
 }
 
 /**
- * Starts the web server, and the ETL process.
- * If the `--startmeup` flag is passed, the ETL process will start immediately.
+ * Inicia el servidor web y el proceso ETL.
+ * Si se pasó el flag `--startmeup`, el proceso ETL se iniciará inmediatamente.
  */
 async function main(): Promise<void> {
-  startWebserver()
-  etlSchedule.start()
-  if (startmeup){
-    await startETL()
-  }else{
-    console.log('ETL process will start on scheduled time')
+  startWebserver();
+  etlSchedule.start();
+
+  if (startmeup) {
+    await startETL();
+  } else {
+    console.log('El proceso ETL comenzará en el horario programado');
   }
 }
 
 /**
- * Iterates over a range of ending digits, downloads and parses a zip file for each digit,
- * and stores the parsed data in a database.
+ * Itera sobre un rango de dígitos finales, descarga y parsea un archivo ZIP para cada dígito,
+ * y almacena los datos parseados en la base de datos.
  */
 async function startETL(): Promise<void> {
   for (let endingDigit = 0; endingDigit < 10; endingDigit++) {
-    const data = await downloadAndParseZip(endingDigit)
-    console.log(`${data.length} taxpayers (contribuyentes) found`)
-    if (data.length){
-      await storeData(data)
+    const data = await downloadAndParseZip(endingDigit);
+    console.log(`${data.length} contribuyentes encontrados`);
+    if (data.length) {
+      await storeData(data);
     }
-    console.log(`Done with ending digit: ${endingDigit}`)
+    console.log(`Finalizado con el dígito final: ${endingDigit}`);
   }
 }
 
+/**
+ * Inicia el servidor web en el puerto especificado.
+ */
 function startWebserver(): void {
   app.listen(PORT, () => {
-    console.log(`⚡️ RUC API is listening at http://localhost:${PORT}`)
+    console.log(`⚡️ RUC API está escuchando en http://localhost:${PORT}`);
   });
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,33 +1,46 @@
-import { Router, Request, Response } from 'express'
-import { VERSION, prisma } from '../config'
+import { Router, Request, Response } from 'express';
+import { VERSION, prisma } from '../config';
 
-const router = Router()
+// Inicializa el enrutador de Express
+const router = Router();
 
+/**
+ * Ruta principal que devuelve un mensaje de bienvenida con la versión de la API.
+ */
 router.get('/', (_req: Request, res: Response) => {
-  res.send(`🚀Welcome to the RUC🇵🇾 API v${VERSION}`);
-})
+  res.send(`🚀 Bienvenido a la API de RUC🇵🇾 v${VERSION}`);
+});
 
+/**
+ * Ruta para buscar un contribuyente por su RUC.
+ * @param ruc - El RUC del contribuyente a buscar.
+ */
 router.get('/ruc/:ruc', async (req: Request, res: Response) => {
-  const { ruc } = req.params
+  const { ruc } = req.params;
   const contribuyente = await prisma.contribuyente.findUnique({
-    where: { ruc }
-  })
-  res.send(contribuyente)
-})
+    where: { ruc },
+  });
+  res.send(contribuyente);
+});
 
-// create a route for seeking by razonSocial
+/**
+ * Ruta para buscar contribuyentes por su razón social.
+ * @param razonSocial - La razón social o parte de ella para buscar contribuyentes.
+ */
 router.get('/razon-social/:razonSocial', async (req: Request, res: Response) => {
-  const { razonSocial } = req.params
+  const { razonSocial } = req.params;
   const contribuyentes = await prisma.contribuyente.findMany({
-    where: { razonSocial: { contains: razonSocial } }
-  })
-  res.send(contribuyentes)
-})
+    where: { razonSocial: { contains: razonSocial } },
+  });
+  res.send(contribuyentes);
+});
 
-// 404 fallback route
+/**
+ * Ruta de respaldo para manejar solicitudes a rutas no existentes (404).
+ */
 router.use((_req: Request, res: Response) => {
-  res.status(404).send('🤷‍♂️ Nothing to see here...');
-})
+  res.status(404).send('🤷‍♂️ No hay nada que ver aquí...');
+});
 
-// Export the router
+// Exporta el enrutador para su uso en la aplicación
 export default router;
