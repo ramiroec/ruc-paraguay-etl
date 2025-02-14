615569cfac0213b8c9f66c630bbd16b9ebffc6ac
Ramiro Estigarribia <ramiroec@gmail.com>
2025-02-14 16:27:51 -0300

    Agregar configuración de cron, base de datos y puerto en el archivo .env y mejorar la carga de variables de entorno en la configuración

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,18 @@
-import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
-
-dotenv.config()
-
-export const URL_ROOT = `https://www.dnit.gov.py/documents/20123/662360/ruc`
-export const CRON_SCHEDULE = process.env.CRON_SCHEDULE || '0 */1 * * * *'
-export const DATABASE_URL = process.env.DATABASE_URL || 'file:../data/rucpy.db'
-export const TIMEZONE = process.env.TIMEZONE || 'America/Asuncion'
-export const PORT = process.env.PORT || 8080
-export const VERSION = '0.0.1'
-export const prisma = new PrismaClient()
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+// Carga las variables de entorno desde un archivo .env
+dotenv.config();
+// URL base para descargar los archivos ZIP de RUC
+export const URL_ROOT = `https://www.dnit.gov.py/documents/20123/662360/ruc`;
+// Configuración del cron para programar tareas (por defecto, cada minuto)
+export const CRON_SCHEDULE = process.env.CRON_SCHEDULE || '0 */1 * * * *';
+// URL de la base de datos (por defecto, una base de datos SQLite local)
+export const DATABASE_URL = process.env.DATABASE_URL || 'file:../data/rucpy.db';
+// Zona horaria para el cron (por defecto, Asunción, Paraguay)
+export const TIMEZONE = process.env.TIMEZONE || 'America/Asuncion';
+// Puerto en el que se ejecutará la aplicación (por defecto, 8080)
+export const PORT = process.env.PORT || 8080;
+// Versión de la aplicación
+export const VERSION = '0.0.1';
+// Instancia de Prisma Client para interactuar con la base de datos
+export const prisma = new PrismaClient();
